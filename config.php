4358bdb7d8bcd914476d2bc3dda6c2145dad1ba4
Samir <belkahla.samir@gmail.com>
2019-08-22 10:08:12 +0400

    Update config.php

--- config.php
+++ config.php
@@ -4,16 +4,16 @@
  */
 
 // Database configuration
-define('DB_HOST', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'social_media');
+define('DB_HOST', 'MySQL_Database_Host');
+define('DB_USERNAME', 'MySQL_Database_Username');
+define('DB_PASSWORD', 'MySQL_Database_Password');
+define('DB_NAME', 'MySQL_Database_Name');
 define('DB_USER_TBL', 'users');
 
 // Google API configuration
-define('GOOGLE_CLIENT_ID', '943093687557-7ms1oiui6jr57l750rr1m55pmquamul2.apps.googleusercontent.com');
-define('GOOGLE_CLIENT_SECRET', 'l4wdTwwXwKbfR6IOocSxcjJJ');
-define('GOOGLE_REDIRECT_URL', 'http://localhost/social/');
+define('GOOGLE_CLIENT_ID', 'Insert_Google_Client_ID');
+define('GOOGLE_CLIENT_SECRET', 'Insert_Google_Client_Secret');
+define('GOOGLE_REDIRECT_URL', 'Callback_URL');
 
 // Start session
 if(!session_id()){
